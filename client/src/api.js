8226d5ba1881e8cfc6bc42304275ea9dfbcba578
itay <itay@carwiz.co.il>
2023-07-07 17:06:00 +0300

    Add api requests

---
+++ client/src/api.js
@@ -0,0 +1,20 @@
+import config from './config.json';
+import axios from "axios";
+
+export const getAllGrades = async () => {
+    const response = await axios.get(`${config.api.baseUrl}${config.api.routes.getAllGrades}`);
+
+    return response.data;
+};
+
+export const addNewGrade = async (data) => {
+    const response = await axios.post(`${config.api.baseUrl}${config.api.routes.addNewGrade}`, data);
+
+    return response.data;
+};
+
+export const deleteGrade = async (id) => {
+    const response = await axios.get(`${config.api.baseUrl}${config.api.routes.deleteGrade}/${id}`);
+
+    return response.data;
+};
